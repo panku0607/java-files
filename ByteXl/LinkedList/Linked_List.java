1b45f5a702b64f56e5fb17f81c3fe6cc381c72d8
pankaj thakur <pankajothakur456@gmail.com>
2024-02-22 20:12:46 +0530

    "Linked List"

---
+++ ByteXl/LinkedList/Linked_List.java
@@ -0,0 +1,25 @@
+package ByteXl.LinkedList;
+
+public class Linked_List {
+    
+    class Node{
+        int data ;
+        Node next ;
+         
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head ;
+    public static Node tail; 
+
+
+    public static void main(String[] args) {
+         
+  System.out.println(
+    "geeksforgeeks".indexOf("gee"));
+
+    }
+}
